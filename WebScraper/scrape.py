7aa95e7d79cd19ac15090404cb922904ed0f7cf6
James Dudley <dudleyj2@miamioh.edu>
2018-12-12 01:59:10 -0300

    updated to flake8 standards

--- WebScraper/scrape.py
+++ WebScraper/scrape.py
@@ -16,15 +16,12 @@ for c in ascii_uppercase:
         url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/an' + c + str(i) + '.htm'
         current_page = requests.get(url)
 
-        print(str(c) + " " + str(i))
-
         # continue if we get a 200 response code
         if current_page.status_code is 200:
             pages.append(url)
         else:
             http_status = False
         i += 1
-
 
 
 for item in pages:
@@ -43,22 +40,22 @@ for item in pages:
 
         nation = ""
         years = ""
-        nation_and_years_str = str(artist_info[index+1].string)
-        if nation_and_years_str == "None":
+        nation_years_str = str(artist_info[index+1].string)
+        if nation_years_str == "None":
             nation = "NA"
             years = "NA"
-        elif nation_and_years_str != "":
-            if ',' in nation_and_years_str:
-                nation_and_years = [x.strip() for x in nation_and_years_str.split(',')]
-                nation = nation_and_years[0]
-                years = nation_and_years[1]
-            elif '1' in nation_and_years_str:
-                years = nation_and_years_str
+        elif nation_years_str != "":
+            if ',' in nation_years_str:
+                nation_years = [x.strip() for x in nation_years_str.split(',')]
+                nation = nation_years[0]
+                years = nation_years[1]
+            elif '1' in nation_years_str:
+                years = nation_years_str
             else:
-                nation = nation_and_years_str
-        
+                nation = nation_years_str
+
         artist_link = artist_info[index].find('a')
         link = 'https://web.archive.org' + artist_link.get('href')
-        
+
         f.writerow([name, nation, years, link])
         index += 2
