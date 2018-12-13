b1af8ff2ba912471f51cd031cfecd419acc92c98
James Dudley <dudleyj2@miamioh.edu>
2018-12-13 20:57:21 -0300

    Day 1 Part 2 and Day 2 Parts 1 and 2
    Added the files for the two python scripts that find that answers to
    parts one and two of Day 2 and part two of Day 1. Also contains
    the text files of the input used for the problems.

---
+++ AdventOfCode/day1.py
@@ -0,0 +1,41 @@
+# Advent of Code Challenge
+
+def main():
+    # Day 1 Part 1
+    sum = 0
+    with open('frequencies.txt') as f:
+        for line in f:
+            sum += int(line)
+    print("Part 1: " + str(sum))
+
+    # Day 1 Part 2
+    changes = []
+    with open('frequencies.txt') as f:
+        for line in f:
+            line = line.rstrip('\n')
+            changes.append(line)
+
+    result = False
+    index = 0
+    current_frequency = 0
+    past_frequencies = {str(current_frequency): 1}
+
+    while(result == False):
+        if (index == len(changes)):
+            index = 0
+        item = str(changes[index])
+        if item[0] == '+':
+            current_frequency += int(item[1:])
+        else:
+            current_frequency -= int(item[1:])
+
+        if str(current_frequency) in past_frequencies:
+            print("Part 2: " + str(current_frequency))
+            result = True
+        else:
+            past_frequencies.update({str(current_frequency): 1})
+
+        index += 1
+    
+
+main()
---
+++ AdventOfCode/day2.py
@@ -0,0 +1,51 @@
+# Advent of Code Challenge
+from string import ascii_lowercase
+
+def main():
+    # Day 2 Part 1
+    boxes = []
+    with open('boxes.txt') as b:
+        for line in b:
+            line = line.rstrip('\n')
+            boxes.append(line)
+
+    total_2 = 0
+    total_3 = 0
+    for b in boxes:
+        letter_dict = {x:0 for x in ascii_lowercase}
+        has_2 = False
+        has_3 = False
+
+        for i in range(len(b)):
+            total = letter_dict.get(b[i])
+            total += 1
+            letter_dict.update({b[i]: total})
+        
+        for key in letter_dict:
+            if letter_dict.get(key) == 2:
+                has_2 = True
+            if letter_dict.get(key) == 3:
+                has_3 = True
+        
+        if has_2 == True:
+            total_2 += 1
+        if has_3 == True:
+            total_3 += 1
+
+    print("Part 1: " + str(total_2 * total_3))
+
+    # Day 2 Part 2
+    for b in range(len(boxes) - 1):
+        for o in range(b+1, len(boxes)):
+            id_1 = str(boxes[b])
+            id_2 = str(boxes[o])
+            amount_unequal = 0
+            unmatched_letter_index = 0
+            for i in range(len(id_1)):
+                if (id_1[i] != id_2[i]):
+                    amount_unequal += 1
+                    unmatched_letter_index = i
+            if amount_unequal < 2:
+                print("Part 2: " + id_1[:unmatched_letter_index] + id_1[unmatched_letter_index+1:])        
+        
+main()
