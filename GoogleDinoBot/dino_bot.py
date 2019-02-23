25e765aee25c360f0fdf45586df500c8418de7bd
Jimmy Dudley <dudleyj2@miamioh.edu>
2019-02-23 17:24:46 -0600

    Update for Jump Number
    Including the jump number so that the gif of the program
    running will print out how many jumps it has made.

--- GoogleDinoBot/dino_bot.py
+++ GoogleDinoBot/dino_bot.py
@@ -7,18 +7,21 @@ from PIL import ImageGrab, ImageOps
 
 
 class Coordinates():
-    replay_button = (480,580)
+    replay_button = (480, 580)
     dino = (220, 585)
-    low = (230, 600)
+
 
 def restart_game():
     pyautogui.click(Coordinates.replay_button)
 
+
 def jump(counter):
-    sys.stdout.write("\nJump " + str(counter))
+    print("Jump #" + str(counter))
+    sys.stdout.flush()
     pyautogui.keyDown('space')
     time.sleep(0.05)
     pyautogui.keyUp('space')
+
 
 def image_grab():
     box = (Coordinates.dino[0] + 140, Coordinates.dino[1],
@@ -28,14 +31,16 @@ def image_grab():
     a = array(grayImage.getcolors())
     return a.sum()
 
+
 def main():
     restart_game()
     count = 0
     while True:
         if(image_grab() != 1447):
-            count +=1
+            count += 1
             jump(count)
             time.sleep(0.05)
+
 
 if __name__ == "__main__":
     main()
