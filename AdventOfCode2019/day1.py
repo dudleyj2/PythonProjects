47cd80e6cc56b875c38900e8f71500d1c62659e9
James Dudley <jmdudle2@illinois.edu>
2022-07-14 12:49:12 -0600

    committing old changes

--- AdventOfCode2019/day1.py
+++ AdventOfCode2019/day1.py
@@ -9,18 +9,18 @@ def get_total_fuel(mass):
 def main():
     # Day 1 Part 1
     total_fuel = 0
-    with open('day1p1_input.txt') as f:
+    with open('day1_input.txt') as f:
         for line in f:
             module_mass = int(line)//3 - 2
             total_fuel += module_mass
-    print(f'Part 1: {total_fuel}')
+    print(f'Day 1 Part 1: {total_fuel}')
 
     total_fuel = 0
-    with open('day1p1_input.txt') as f:
+    with open('day1_input.txt') as f:
         for line in f:
             module_mass = int(line)
             module_total_fuel = get_total_fuel(module_mass)
             total_fuel += module_total_fuel
-    print(f'Part 2: {total_fuel}')
+    print(f'Day 1 Part 2: {total_fuel}')
 
 main()
